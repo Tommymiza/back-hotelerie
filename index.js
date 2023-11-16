91111f042ccd5e2c59d547995717a883fe99fbff
Tommymiza <tommymiza20@gmail.com>
2023-11-16 07:03:44 +0300

    Hello

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.get('/vanilla/success', async (req, res) => {
   console.log(req.body);
   res.json({ message: 'success' });
